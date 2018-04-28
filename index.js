a900a2ed993263c345d5c8488d014805bed7df18
Marck Manga <Marck@thinkful.com>
2018-04-28 16:19:42 -0700

    a11y refactor

--- index.js
+++ index.js
@@ -3,30 +3,53 @@ const apiKey = "AIzaSyCLca5LG3szn-Q24AmBOWyytenluU6DbQU"
 
 
 //GET DATA FROM API FUNCTION 
-function getDatafromAPI(searchTerm, callback) {
+function getDatafromAPI(searchTerms, callback) {
 	const settings = {
 		url: YT_SEARCH_URL,
 		data: {
 			part: 'snippet',
 			key: apiKey,
-			q: `${searchTerm} in:name`
+			q: `${searchTerms} in:name`
 		},
 		dataType: 'json',
 		type: 'GET',
 		success: callback
 	};
-	$.ajax(settings);
+$.ajax(settings);
 }
 
 
 //RENDER RESULTS FUNCTION 
 function renderResults(result) {
-
+	return `
+	<div>
+		<h2>
+		<a href="https://www.youtube.com/watch?v=${result.id.videoId}">${result.snippet.title}</a>
+		</h2>
+		<a href="https://www.youtube.com/watch?v=${result.id.videoId}" target="_blank"><img alt="search-result-img" src="${result.snippet.thumbnails.medium.url}"></a>
+	</div>  
+	`;
 }
 
 
 //DISPLAY-APPEND TO HTML DATA AS RESULTS FUNCTION 
+function displayYouTubeResults(data) {
+	let results = data.items.map((item, index) => renderResults(item));
+	$('.search-results').html(results);
+}
 
 
 
 //EVENT LISTENER FOR SUBMIT 
+function listenSubmit () {
+	$('#search-form').submit(event => {
+		event.preventDefault();
+		let searchTerms = document.getElementById('js-query').value;
+		console.log(searchTerms);
+		getDatafromAPI(searchTerms, displayYouTubeResults);
+	})
+}
+
+listenSubmit();
+
+
